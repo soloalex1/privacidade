1dbb104579cde3ba1acfa61bd13fd0ef0971d851
Alexandre Machado <amch.smd@gmail.com>
2019-09-11 14:49:23 -0300

    WIP: Tratamento de atributos
    Tô no estágio fazendo isso escondido, então aguarde muitos commits com pouco código. Também não tô conseguindo testar aqui, qualquer coisa se quebrar é culpa minha mesmo. Dei um drop no id e alterei a exibição do gênero.

--- k_anonimato/k_anonimato.py
+++ k_anonimato/k_anonimato.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pandas as pd
 
+# lendo dataset
 df = pd.read_csv('salario.csv')
-df.head().groupby("pais").mean()
+
+# removendo o identificador explícito
+df.drop(columns=["id"])
+
+# codificando gênero pra {0, 1}
+encoder = [(["genero"], LabelEncoder())]
+mapper = DataFrameMapper(encoder, df_out=True)
+col_novas = mapper.fit_transform(df.copy())
+
+df.shape()
+df.head()
+
+export_csv = df.to_csv(r'salario_2.csv', index=None, header=True)
